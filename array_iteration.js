8900b3718c74d0853d9c632dbbd3e5f882e06324
sagarhasan273 <sagarhasan273@gmail.com>
2022-05-04 22:52:10 +0600

    let x of f

--- array_iteration.js
+++ array_iteration.js
@@ -3,9 +3,19 @@ let txt = '';
 
 array.forEach(myFunction);
 
-function myFunction(value, index, array){
+function myFunction(value, index, array) {
     txt += value + "\n";
     console.log(array)
     console.log(index)
 }
+function myFunction1() {
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    const f = fruits.entries();
+    
+    for (let x of f) {
+        document.getElementById("demo").innerHTML += x + "<br>";
+        console.log(x)
+    }
+}
+
 console.log(txt);
