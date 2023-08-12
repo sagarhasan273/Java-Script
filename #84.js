16a99851fc98c7ef3ff6685966526fd40261448b
sagarhasan273 <sagarhasan273@gmail.com>
2023-08-12 18:32:42 +0600

    Promise & asynchronous

---
+++ #84.js
@@ -0,0 +1,48 @@
+const enrollProcess = false;
+const marks = 70;
+
+function enroll(){
+    console.log("Enrollment is in process");
+    const promise = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            if (enrollProcess){
+                resolve();
+            }else{
+                reject("Payment Failed!");
+            }
+        }, 2000);
+    });
+    return promise;
+}
+
+function process(){
+    console.log("Checking Marks!");
+    const promise = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            if (marks >= 80){
+                resolve();
+            }else{
+                reject("Marks is not as required!")
+            }
+        }, 3000);
+    });
+
+    return promise;
+}
+
+function getCirtified(){
+    const promise = new Promise(function(resolve){
+        resolve("cirtified");
+    })
+    return promise
+}
+
+enroll()
+    .then(process)
+    .then(getCirtified)
+    .then(function(value){
+        console.log(value);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
